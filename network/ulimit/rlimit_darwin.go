da4a9c7d0c24157b3c4bfe4cfb8abb361b7439fa
njmdk <5673366@qq.com>
2023-11-02 11:30:11 +0800

    1.增加darwin系统支持

---
+++ network/ulimit/rlimit_darwin.go
@@ -0,0 +1,35 @@
+//go:build darwin
+// +build darwin
+
+package ulimit
+
+import (
+	"sync"
+	"syscall"
+)
+
+var rLimit sync.Once
+
+func SetRLimit() error {
+	var err error
+
+	rLimit.Do(func() {
+		err = setRLimit()
+	})
+
+	return err
+}
+
+func setRLimit() error {
+	var rLimit syscall.Rlimit
+
+	rLimit.Cur = 999999
+	rLimit.Max = 999999
+
+	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
